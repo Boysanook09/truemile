a7d60d529b2af149312db7f1e83b8bc882f90cc2
Boysanook09 <boysanook7890@gmail.com>
2025-06-18 00:30:49 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById('clickMeBtn').addEventListener('click', function() {
+    document.getElementById('message').textContent = 'You clicked the button!';
+});
+
+document.getElementById('loginBtn').addEventListener('click', function() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const loginMessage = document.getElementById('loginMessage');
+    if (username === 'admin' && password === '1234') {
+        loginMessage.style.color = '#0078d7';
+        loginMessage.textContent = 'Login successful!';
+        setTimeout(function() {
+            window.location.href = 'admin.html';
+        }, 800);
+    } else if (username === 'user' && password === '1234') {
+        loginMessage.style.color = '#0078d7';
+        loginMessage.textContent = 'Login successful!';
+        setTimeout(function() {
+            window.location.href = 'storage.html';
+        }, 800);
+    } else {
+        loginMessage.style.color = '#d32f2f';
+        loginMessage.textContent = 'Invalid username or password';
+    }
+});
